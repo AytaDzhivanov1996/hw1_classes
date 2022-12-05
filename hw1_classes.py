cd825b574e2ee8be584d0e7ff3be4a0c2572a9e8
AytaDzhivanov1996 <kurt.alive.1994@gmail.com>
2022-12-05 14:48:16 +0300

    tasks done

---
+++ hw1_classes.py
@@ -0,0 +1,17 @@
+class Bottle:
+    color = ''
+    volume = 0.0
+
+
+bottle_1 = Bottle()
+bottle_1.color = 'Красная'
+bottle_1.volume = 0.7
+bottle_2 = Bottle()
+bottle_2.color = 'Белая'
+bottle_2.volume = 0.3
+bottle_3 = Bottle()
+bottle_3.color = 'Черная'
+bottle_3.volume = 0.1
+print(bottle_1.color, bottle_1.volume)
+print(bottle_2.color, bottle_2.volume)
+print(bottle_3.color, bottle_3.volume)
---
+++ hw1_classes_task2.py
@@ -0,0 +1,25 @@
+class Bottle:
+    color = ''
+    contains = 0.0
+
+    def __init__(self):
+        self.contains = int(self.contains)
+
+    def get_content(self):
+        return self.contains
+
+    def fill(self, volume):
+        self.contains = self.contains + volume
+        return self.contains
+
+
+bottle_1 = Bottle()
+bottle_2 = Bottle()
+bottle_1.color = 'Красная'
+bottle_2.color = 'Синяя'
+print(bottle_1.color, bottle_1.get_content())
+bottle_1.fill(100)
+print(bottle_1.color, bottle_1.get_content())
+print(bottle_2.color, bottle_2.get_content())
+bottle_2.fill(500)
+print(bottle_2.color, bottle_2.get_content())
